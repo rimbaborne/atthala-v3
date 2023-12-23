792d06583cc28ffa3f00d657adfd2c229f971ddb
rimbaborne <gagangborne@gmail.com>
2023-12-24 07:53:40 +0800

    seting responsive layout

--- resources/views/components/layouts/main.blade.php
+++ resources/views/components/layouts/main.blade.php
@@ -1,5 +1,5 @@
 <x-layouts.nav />
-<div class="flex pt-16 overflow-hidden bg-gray-50">
+<div class="flex overflow-hidden bg-gray-50">
     <x-layouts.sidebar />
 
     <div id="main-content" class="relative w-full h-full overflow-y-auto bg-gray-50 lg:ml-64">
