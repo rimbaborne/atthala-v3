7a1a385f69b3d8fbb4aa235f426187fdf37be4ee
rimbaborne <gagangborne@gmail.com>
2023-12-25 12:21:30 +0800

    fungsi super admin & seting toast store

--- app/Http/Controllers/SuperAdminController.php
+++ app/Http/Controllers/SuperAdminController.php
@@ -6,6 +6,9 @@ use Illuminate\Http\Request;
 use ProtoneMedia\Splade\SpladeTable;
 use App\Models\User;
 use Maatwebsite\Excel\Excel;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use ProtoneMedia\Splade\Facades\Toast;
 
 class SuperAdminController extends Controller
 {
@@ -37,5 +40,47 @@ class SuperAdminController extends Controller
         // ->export()
         ->paginate(15),
         ]);
+    }
+
+    public function coa() {
+        $roles = SpladeTable::for(Role::class)
+        ->column('name')
+        ->paginate(10);
+
+        $permissions = Permission::all();
+
+        return view('dashboard.super-admin.coa.index', compact('roles', 'permissions'));
+    }
+
+    public function roles() {
+        $roles = SpladeTable::for(Role::class)
+                ->column('name')
+                ->column('actions')
+                ->paginate(10);
+
+        $permissions = Permission::all();
+
+        return view('dashboard.super-admin.roles.index', compact('roles', 'permissions'));
+    }
+
+    public function roles_create() {
+        return view('dashboard.super-admin.roles.create');
+    }
+
+    public function roles_store() {
+        request()->validate([
+            'name' => 'required|string|min:3|max:50',
+        ]);
+
+        try {
+            $role = Role::create(['name' => request()->name]);
+
+            Toast::title('Data Berhasil Ditambahkan');
+            return redirect()->route('superadmin.roles.index');
+        } catch (\Throwable $th) {
+            Toast::warning('Terjadi Kesalahan, Mohon Ulangi');
+            return redirect()->route('superadmin.roles.index');
+        }
+
     }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use ProtoneMedia\Splade\Facades\Splade;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Splade::defaultToast(function ($toast) {
+            $toast->info()->leftTop()->autoDismiss(10);
+        });
     }
 }
