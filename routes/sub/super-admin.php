17a0db19b741d84c639ece20f3069bebe0d3a267
rimbaborne <rimbaborne@gmail.com>
2023-12-31 06:50:14 +0800

    fix route super admin

--- routes/sub/super-admin.php
+++ routes/sub/super-admin.php
@@ -2,20 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SuperAdminController;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 
 Route::get('/', [SuperAdminController::class, 'index'])->name('superadmin.index');
 Route::get('/coa', [SuperAdminController::class, 'coa'])->name('superadmin.coa.index');
 
-Route::get('/roles', [SuperAdminController::class, 'roles'])->name('superadmin.roles.index');
+Route::get('/roles', [SuperAdminController::class, 'roles_index'])->name('superadmin.roles.index');
 Route::get('/roles/create', [SuperAdminController::class, 'roles_create'])->name('superadmin.roles.create');
 Route::post('/roles/store', [SuperAdminController::class, 'roles_store'])->name('superadmin.roles.store');
 
-Route::get('/users', [SuperAdminController::class, 'users'])->name('superadmin.users.index');
+Route::get('/users', [SuperAdminController::class, 'users_index'])->name('superadmin.users.index');
 Route::get('/users/create', [SuperAdminController::class, 'users_create'])->name('superadmin.users.create');
 Route::post('/users/store', [SuperAdminController::class, 'users_store'])->name('superadmin.users.store');
 
-Route::get('/log-dml', [SuperAdminController::class, 'log_dml'])->name('superadmin.log-dml');
+Route::get('/log-dml', [SuperAdminController::class, 'log_dml_index'])->name('superadmin.log-dml.index');
 
-Route::get('/divisi', [SuperAdminController::class, 'divisi'])->name('superadmin.divisi');
+Route::get('/divisi', [SuperAdminController::class, 'divisi_index'])->name('superadmin.divisi.index');
 
