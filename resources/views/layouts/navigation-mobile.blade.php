cb5947090638a0111b6197dc2a182a6bf7b2000e
asif <rimbaborne@gmail.com>
2024-08-19 17:27:24 +0800

    --

--- resources/views/layouts/navigation-mobile.blade.php
+++ resources/views/layouts/navigation-mobile.blade.php
@@ -14,7 +14,7 @@
                     Dashboard
                 </span>
             </Link>
-            <Link href="/data" type="button"
+            <Link href="/dashboard/data" type="button"
                 class="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group">
                 <svg class="w-5 h-5 mb-2 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500"
                     aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
@@ -26,7 +26,7 @@
                     Data
                 </span>
             </Link>
-            <Link href="/riwayat-pembayaran" type="button"
+            <Link href="/dashboard/riwayat-pembayaran" type="button"
                 class="inline-flex flex-col items-center justify-center px-5 border-e border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-800 group dark:border-gray-600">
                 <svg class="w-5 h-5 mb-2 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500"
                     aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
